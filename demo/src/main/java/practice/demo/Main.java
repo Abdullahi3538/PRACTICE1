79bdb5819c9a6c63c532c9e5452f5a85efcfe231
ZuhurAbdi22 <kppquen45@gmail.com>
2026-06-29 20:51:38 +0300

    waxaan kuso dry shaqo kle

--- demo/src/main/java/practice/demo/Main.java
+++ demo/src/main/java/practice/demo/Main.java
@@ -9,6 +9,7 @@ public class Main {
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 		System.out.println("my name is nasri");
+		System.out.println("my name is Zuhur");
 	}
 
 }
